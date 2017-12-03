931ccb383837d391ed5b9083dc20061b8b3515ad
xcoreashu <connorashutosh@gmail.com>
2017-12-03 13:59:25 +0530

    First commit

---
+++ music/static/music/main.js
@@ -0,0 +1,88 @@
+<script>
+    
+    var slideIndex = 0;
+    showSlides();
+
+
+    function showSlides(){
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++){
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+        setTimeout(showSlides , 2000);
+
+
+    }
+
+
+var AlbumsListPage = {
+	init: function() {
+		this.$container = $('.albums-container');
+		this.render();
+		this.bindEvents();
+	},
+
+	render: function() {
+
+	},
+
+	bindEvents: function() {
+		$('.btn-favorite', this.$container).on('click', function(e) {
+			e.preventDefault();
+
+			var self = $(this);
+			var url = $(this).attr('href');
+			$.getJSON(url, function(result) {
+				if (result.success) {
+					$('.glyphicon-star', self).toggleClass('active');
+				}
+			});
+
+			return false;
+		});
+	}
+};
+
+var SongsListPage = {
+	init: function() {
+		this.$container = $('.songs-container');
+		this.render();
+		this.bindEvents();
+	},
+
+	render: function() {
+
+	},
+
+	bindEvents: function() {
+		$('.btn-favorite', this.$container).on('click', function(e) {
+			e.preventDefault();
+
+			var self = $(this);
+			var url = $(this).attr('href');
+			$.getJSON(url, function(result) {
+				if (result.success) {
+					$('.glyphicon-star', self).toggleClass('active');
+				}
+			});
+
+			return false;
+		});
+	}
+};
+
+$(document).ready(function() {
+	AlbumsListPage.init();
+	SongsListPage.init();
+});
+</script>
